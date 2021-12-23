f3be09725f4bd27b166a8f8e6dff7cbb06c587db
Ronin <rlombardino44@gmail.com>
2021-12-23 10:26:24 -0800

    added file for Q&A component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import QandA from './Q&A/ModuleMain.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +11,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1>FEC Testing</h1>
+      <div>
+        <h1>FEC Testing</h1>
+        <QandA />
+      </div>
     );
   }
 }
---
+++ client/src/components/Q&A/ModuleMain.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class QandA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div> Q and A </div>
+    );
+  }
+}
+
+export default QandA;
