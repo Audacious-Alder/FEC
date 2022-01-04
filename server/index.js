1a0713f3cf17c359dd474fba7a46fd1c5b2c1cbe
Jesse Huang <jjhuang417@gmail.com>
2022-01-04 09:34:38 -0800

    Revert "Merge pull request #38 from Audacious-Alder/Questions"
    This reverts commit 02d3d8e854983276738a62cd490c860f6805ea3f, reversing
    changes made to f648a403ca3d2909cdfbd97980c078214cef3972.

--- server/index.js
+++ server/index.js
@@ -12,13 +12,6 @@ app.use(express.json());
 app.get('/products', api.products.productGET);
 app.get('/products/styles', api.products.productStylesGET);
 
-// Questions
-app.get('/questions', api.questions.questionsGET);
-app.get('/questions', api.questions.questionsGET);
-app.get('/questions/answers', api.questions.answersGET);
-app.post('/questions', api.questions.questionPOST);
-app.post('/questions/answers', api.questions.answersPOST);
-
 // Related
 app.get('/related', api.products.productRelatedGET);
 app.get('/related/products', api.products.multiProductGET);
